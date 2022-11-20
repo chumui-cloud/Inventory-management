bcb3f84cacfb45019c3ee285d873090363a5963c
chumui-cloud <chumuitripura12@gmail.com>
2022-11-20 20:40:23 +0600

    Change input product functionality

--- Inventory-management.cpp
+++ Inventory-management.cpp
@@ -16,6 +16,7 @@ public:
 	void inputProduct();
 	void displayProduct();
 };
+
 void Product::inputProduct()
 {
 	cout << "Enter Product Name: ";
@@ -31,12 +32,14 @@ void Product::inputProduct()
 	cout << endl;
 	system("cls");
 }
+
 void Product::displayProduct()
 {
 
 	cout << ProductName << setw(15) << Quantity << setw(15) << Price << endl;
 
 }
+
 int main()
 {
 	vector<Product> products;
@@ -57,31 +60,25 @@ int main()
 		
 		{
 			system("cls");
-			Product newProduct;
-			newProduct.inputProduct();
-			products.push_back(newProduct);
-			cout << "Do you want to add more?(If yes type y/Y else type any key.)" << endl;
 			string ans;
-			cin >> ans;
 			while (true)
 			{
-				if (ans == "y" || ans == "Y")
+				Product newProduct;
+				newProduct.inputProduct();
+				products.push_back(newProduct);
+				cout << "Do you want to add more?(If yes type y/Y else type any key.)" << endl;
+				cin >> ans;
+				if (ans != "y" && ans != "Y")
 				{
-					Product newProduct;
-					newProduct.inputProduct();
-					products.push_back(newProduct);
-					cout << "Do you want to add more?(If yes type y/Y Else type any letter.)"<<endl;
-					string ans;
-					cin >> ans;
-				}
-				break;
+					break;
+				}				
 			}
 				break;
 			}
 		case 2:
 		{
 			system("cls");
-			cout << "Product-Name " << setw(10) << "Quantity " << setw(10) << "Price" << endl;
+			cout << "Product-Name" << setw(10) << "Quantity" << setw(10) << "Price" << endl;
 			for (int j = 0; j < products.size(); j++)
 			{
 				products[j].displayProduct();
